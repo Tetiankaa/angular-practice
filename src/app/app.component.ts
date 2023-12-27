b3ffe3eb16ec81169eb641fd9c9e67de097e5079
Tetiankaa <tanyakorsun0208@gmail.com>
2023-12-27 09:27:28 +0100

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {getSymbolIterator} from "rxjs/internal/symbol/iterator";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'practice';
+}
